0e351a981a1c0357e8bd2007bc404825058dec92
godsroom009 <godsroomdevelopment@gmail.com>
2025-02-21 18:42:15 +0530

    Update TaskRepository.java

--- src/main/java/com/example/quizapp/repository/TaskRepository.java
+++ src/main/java/com/example/quizapp/repository/TaskRepository.java
@@ -1,9 +1,13 @@
 package com.example.quizapp.repository;
 
-import com.example.quizapp.model.Task;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.quizapp.model.Task;
+
 @Repository
 public interface TaskRepository extends MongoRepository<Task, String> {
+    List<Task> findByBatchId(String batchId);
 }
